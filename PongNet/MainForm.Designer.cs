94677839bf0d9176da6582550ac8bb7dceecd497
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2016-02-19 09:46:49 +0100

    First commit.

---
+++ PongNet/MainForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PongNet
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvasPanel = new System.Windows.Forms.Panel();
+            this.platformBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.canvasPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.platformBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // canvasPanel
+            // 
+            this.canvasPanel.Controls.Add(this.platformBox);
+            this.canvasPanel.Location = new System.Drawing.Point(0, 27);
+            this.canvasPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.canvasPanel.Name = "canvasPanel";
+            this.canvasPanel.Size = new System.Drawing.Size(484, 522);
+            this.canvasPanel.TabIndex = 0;
+            // 
+            // platformBox
+            // 
+            this.platformBox.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.platformBox.Location = new System.Drawing.Point(0, 440);
+            this.platformBox.Margin = new System.Windows.Forms.Padding(0);
+            this.platformBox.Name = "platformBox";
+            this.platformBox.Size = new System.Drawing.Size(484, 40);
+            this.platformBox.TabIndex = 0;
+            this.platformBox.TabStop = false;
+            this.platformBox.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintCanvas);
+            this.platformBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MovePlatform);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(484, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(484, 561);
+            this.Controls.Add(this.canvasPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 600);
+            this.MinimumSize = new System.Drawing.Size(500, 600);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.canvasPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.platformBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvasPanel;
+        private System.Windows.Forms.PictureBox platformBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+    }
+}
+
---
+++ PongNet/MainForm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PongNet
+{
+    public partial class MainForm : Form
+    {
+
+        private Timer timer = new Timer();
+        private Graphics graphics;
+        private Rectangle platform;
+        private Rectangle ball;
+        private int axisXSpeed;
+        private int axisYSpeed;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            platform = new Rectangle(182, 400, 120, 40);
+            ball = new Rectangle(50, 50, 25, 25);
+            axisXSpeed = 5;
+            axisYSpeed = 5;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            graphics = canvasPanel.CreateGraphics();
+            timer.Interval = 60;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        private void PaintCanvas(object sender, PaintEventArgs e)
+        {
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            graphics.FillRectangle(new SolidBrush(Color.RoyalBlue), platform);
+            graphics.FillEllipse(new SolidBrush(Color.Red), ball);
+        }
+
+        private void MovePlatform(object sender, MouseEventArgs e)
+        {
+            platform.X = e.X - 60;
+            Refresh();
+        }
+
+        private void MoveBall()
+        {
+            if (ball.X + axisXSpeed < 0)
+            {
+                axisXSpeed += 10;
+            }
+            if (ball.X + axisXSpeed >= platformBox.Width)
+            {
+                axisXSpeed -= 10;
+            }
+            if (ball.Y + axisYSpeed < 0)
+            {
+                axisYSpeed += 5;
+                if (axisYSpeed == 0)
+                {
+                    axisYSpeed += 5;
+                }
+            }
+            if (ball.Y  >= platform.Y)
+            {
+                axisXSpeed = 0;
+                axisYSpeed = 0;
+                timer.Stop();
+
+                var gameOver = MessageBox.Show("GAME OVER", "Message", MessageBoxButtons.OK);
+                if (gameOver == System.Windows.Forms.DialogResult.OK)
+                {
+                    Application.Restart();
+                }
+            }
+            if (ball.IntersectsWith(platform))
+            {
+                axisYSpeed -= 10;
+            }
+            ball.X += axisXSpeed;
+            ball.Y += axisYSpeed;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            MoveBall();
+            Refresh();
+        }
+    }
+}
